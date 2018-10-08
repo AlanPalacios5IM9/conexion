94de11be60d85615fcec14036a6805d0a69ed3e6
AlanPalacios5IM9 <42345667+AlanPalacios5IM9@users.noreply.github.com>
2018-10-08 08:54:04 -0500

    Add files via upload

---
+++ src/java/mx/edu/ipn/cecyt9/projecc/model/Rol.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.projecc.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Alumno
+ */
+@Entity
+@Table(name = "rol")
+@NamedQueries({
+    @NamedQuery(name = "Rol.findAll", query = "SELECT r FROM Rol r")})
+public class Rol implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_rol")
+    private Integer idRol;
+    @Size(max = 45)
+    @Column(name = "descripcion")
+    private String descripcion;
+    @JoinColumn(name = "id_registro", referencedColumnName = "id_registro")
+    @ManyToOne
+    private Registro idRegistro;
+
+    public Rol() {
+    }
+
+    public Rol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Registro getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(Registro idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRol != null ? idRol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol)) {
+            return false;
+        }
+        Rol other = (Rol) object;
+        if ((this.idRol == null && other.idRol != null) || (this.idRol != null && !this.idRol.equals(other.idRol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.projecc.model.Rol[ idRol=" + idRol + " ]";
+    }
+    
+}
